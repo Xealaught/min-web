d5532fe5a4ab226bb66f5b36d344eb0fe4c49c4a
Tucker Alexander <tuckergalexander@gmail.com>
2018-04-09 01:23:53 -0600

    Test

--- njos57IJf-0/index.php
+++ njos57IJf-0/index.php
@@ -8,10 +8,13 @@ function getIpAddress() {
         return $_SERVER['REMOTE_ADDR'];
     }
 }
-$myfile = fopen("ip-list.php", "a");
-$clientIp = getIpAddress() . PHP_EOL;
-fwrite($myfile, $clientIp);
-fclose($myfile);
+$file = 'ip-list.php';
+// Open the file to get existing content
+$current = file_get_contents($file);
+// Append a new person to the file
+$current .= getIpAddress() . PHP_EOL;
+// Write the contents back to the file
+file_put_contents($file, $current);
 header("Location: https://www.youtube.com/watch?v=njos57IJf-0");
 die();
 ?>
