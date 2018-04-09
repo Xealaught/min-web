a101eee4cbc774d3f0303beb559d07b33fcdca5f
Tucker Alexander <tuckergalexander@gmail.com>
2018-04-08 23:51:20 -0600

    Test

---
+++ tickle-tickle.php
@@ -0,0 +1,12 @@
+<?php
+function getIpAddress() {
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipAddresses = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        return trim(end($ipAddresses));
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+echo getIpAddress();
+?>
